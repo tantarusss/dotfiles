063b855cfa6e97086fa198346ac370c01284dbdf
Benedict Volz <benedict.volz@web.de>
2024-02-17 13:27:31 +0100

    new changes

--- nvim/lua/custom/plugins/autopairs.lua
+++ nvim/lua/custom/plugins/autopairs.lua
@@ -1,10 +1,8 @@
 return {
   "windwp/nvim-autopairs",
-  -- Optional dependency
   dependencies = { 'hrsh7th/nvim-cmp' },
-  config = function()
+  config = function ()
     require("nvim-autopairs").setup {}
-    -- If you want to automatically add `(` after selecting a function or method
     local cmp_autopairs = require('nvim-autopairs.completion.cmp')
     local cmp = require('cmp')
     cmp.event:on(
@@ -13,3 +11,4 @@ return {
     )
   end,
 }
+
